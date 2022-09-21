171bc655cbe42f93da188da00bfed447265ce03d
Wubsera <wubsera21@gmail.com>
2022-09-20 19:50:55 -0700

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
-""" #!/usr/bin/python3 """
+#!/usr/bin/python3
 def islower(c):
-    codeUnicode = ord(c)
-            if (codeUnicode >= 97) and (codeUnicode <= 122):
-                return True
-            return False
+    return(ord('a') <= ord(c) <= ord('z'))
