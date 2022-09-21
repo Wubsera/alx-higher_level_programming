8a9904364b4a2a6cdb926479106985106843a469
Wubsera <wubsera21@gmail.com>
2022-09-21 06:47:40 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(
-                "{}{}".format(i, j),
-                end=", " if int(str(i) + str(j)) < 89 else "\n"
-                )
+def uppercase(str):
+    tmp = list(str)
+            for i in range(len(tmp)):
+                if (ord(tmp[i]) > 96 and ord(tmp[i]) < 123):
+                    tmp[i] = chr(ord(tmp[i]) - 32)
+                                            print("{}".format("".join(tmp)))
