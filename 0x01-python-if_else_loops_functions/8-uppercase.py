6b7bdf33230ab9cc349afde8e57fcbae72c6984e
Wubsera <wubsera21@gmail.com>
2022-09-21 06:43:11 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,4 @@
-#!/usr/bin/pyth in range(10):
-    for j in range(i + 1, 10):
-                print(
-                                    "{}{}".format(i, j),
-                                                end=", " if int(str(i) + str(j)) < 89 else "\n"
-                                                            )n3
+#!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
         print(
