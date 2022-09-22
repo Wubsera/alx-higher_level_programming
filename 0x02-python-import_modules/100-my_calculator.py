129d0594bac7374579e6bf90c293cf632bed6122
Wubsera <wubsera21@gmail.com>
2022-09-22 06:59:59 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-            from sys import argv
-
-                if len(argv) != 4:
-                    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-                                    exit(1)
-
-                                        numb1 = int(argv[1])
-                                            numb2 = int(argv[3])
-
-                                                if argv[2] not in ["+", "-", "*", "/"]:
-                                                    print("Unknown operator. Available operators: +, -, * and /")
-                                                                    exit(1)
-                                                                elif argv[2] is "+":
-                                                                    print("{} + {} = {}".format(numb1, numb2, add(numb1, numb2)))
-                                                                elif argv[2] is "-":
-                                                                    print("{} - {} = {}".format(numb1, numb2, sub(numb1, numb2)))
-                                                                elif argv[2] is "*":
-                                                                    print("{} * {} = {}".format(numb1, numb2, mul(numb1, numb2)))
-                                                                elif argv[2] is "/":
-                                                                    print("{} / {} = {}".format(numb1, numb2, div(numb1, numb2)))
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
+        a = int(argv[1])
+        b = int(argv[3])
+        ops = ["+", "-", "*", "/"]
+        from calculator_1 import add, sub, mul, div
+        funcs = [add, sub, mul, div]
+        for i, s in enumerate(ops):
+            if argv[2] == s:
+                print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
+                break
+            else:
+                print("Unknown operator. Available operators: +, -, * and /")
+                quit(1)
