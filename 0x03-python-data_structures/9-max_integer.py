168443999acf3e3617b3dbdbd8247f7bd3580b3d
Wubsera <wubsera21@gmail.com>
2022-09-27 05:52:45 -0700

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if my_list:
         max = my_list[0]
-                            for elm in my_list:
-                                if elm > max:
-                                    max = elm
-                                                                        return max
-                                                                    return None
+        for elm in my_list:
+            if elm > max:
+                max = elm
+                return max
+            return None
