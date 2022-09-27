e01eceec3f9eed6f3cfab568facb3e5c38365ac2
Wubsera <wubsera21@gmail.com>
2022-09-27 06:12:20 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    string = ""
-            for c in my_string:
-                if c != 'c' and c != 'C':
-                    string = string + c
-                                            return string
+    ret = ""
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            ret += my_string[i]
+            return ret
